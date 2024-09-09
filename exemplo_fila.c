99a2118d2a7fa10ce01ac5dff31209610591e06b
Laryssa Esper Pereira <laryssaesperpereira@gmail.com>
2024-09-09 15:23:50 -0300

    add files

---
+++ exemplo_fila.c
@@ -0,0 +1,57 @@
+#include <stdio.h> //padrão
+#include <stdlib.h> //ponteiro
+
+#define TAM 10 //constante
+
+//definir struct
+typedef struct{
+	int R; //retaguarda
+	int F; //frente
+	int vet[TAM];
+}filavet;
+
+//função enfileirar
+//void = não retorna nada para a main, só executa
+void enfileirar(int valor, filavet*p){
+	//verificar se a fila está cheia
+	//fila cheia quando o R chega ao limite (tamanho do vetor)
+	if(p->R==TAM-1){
+		printf("\nFila cheia");
+		exit(0);
+	}
+	p->R++; //incrementa R
+	p->vet[p->R]=valor; //Armazena valor dentro do vetor (fila)
+}
+
+//função desenfileirar
+//int = retorna para main
+int desenfileirar(filavet*p){
+	int aux; //variável auxiliar para se comunicar com a main
+	//F==TAM, pois se for TAM-1, não retira o último a ser colocado na fila
+	if(p->F==TAM){
+		printf("\nFila vazia");
+		exit(1);
+	}
+	aux=p->vet[p->F]; //desenfileira o vetor 
+	p->F++; //incrementa F, pois fica embaixo e precisa subir até nivelar com R
+	return aux; //retorna valor para a main
+}
+
+//função main
+int main (void) {
+	filavet fila; //declarar struct
+	int valor; //variável que recebe o valor digitado
+	fila.R=-1;
+	fila.F=0;
+	//chama função enfileirar
+	for(int i=0; i<TAM; i++){
+		printf("Digite o valor a ser enfileirado: \n");
+		scanf("%d", &valor);
+		enfileirar(valor, &fila);		
+	}
+	//chama função desenfileirar
+	for(int i=0; i<TAM; i++){
+		printf("\n%d", desenfileirar(&fila));
+	}
+	return 0;
+}
---
+++ exemplo_pilha.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+//fazer uma pilha com 10 posições de inteiro
+
+//constante
+#define TAM 10 //#define nome conteúdo
+
+//define a struct
+typedef struct{
+    int vet[TAM]; //vetor com o tamanho da pilha
+    int topo; //variável de topo da pilha
+}pilhavet; //nome da struct
+
+//criar função para empilhar
+//void (não retorna nada)
+//parâmetros=valor que será inserido, ponteiro para struct pilhavet
+
+void empilha(int valor, pilhavet*p){
+    if(p->topo==TAM-1){
+        //verifica se a pilha está cheia
+        //TAM-1, pois o vetor vai de 0 a 9 e TAM é 10 (o usuário entende de 1 a 10)
+        printf("\nPilha cheia");
+        exit(0);
+    }
+    p->topo++; //incrementa topo
+    p->vet[p->topo]=valor; //guarda o valor no topo
+}
+
+//criar função para desempilhar
+int desempilha(pilhavet*p){
+    int aux; //variável auxiliar
+    
+    if(p->topo<0){
+        //verifica se a pilha está vazia
+        printf("\nPilha vazia");
+        exit(1); //exit(1); pois é o segundo exit do código
+    }
+    
+    aux=p->vet[p->topo]; 
+    p->topo--; //decrementa topo
+    return aux; //retorna auxiliar para main
+}
+
+//criar função principal
+int main(void){
+    pilhavet pilha; //declara struct com nome pilha
+    int valor; //valor a ser adicionado pelo usuário
+    pilha.topo=-1; //inicia a pilha com valor -1 (começa do 0)
+    
+    //carregar pilha com for
+    for(int i=0; i<TAM; i++){
+        printf("\nDigite p valor a ser empilhado: ");
+        scanf("%d", &valor);
+        empilha(valor,&pilha); //chama função empilha e aloca os valores na struct com nome pilha
+        //ao invés de seta para o ponteiro, chama o & para endereço de memória
+    }
+    
+    //descarregar pilha
+    for(int i=0; i<TAM; i++){
+        printf("\n%d", desempilha(&pilha)); //chama a função desempilha e aloca na struct
+    }
+    
+    printf("\n");
+    system("pause");
+}
